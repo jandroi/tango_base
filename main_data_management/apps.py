626af5d92a731a48075d1f08a56a3f9d80acddf0
jandroi <bosnovick@gmail.com>
2026-09-06 16:32:41 +0200

    tango base cleaned

--- main_data_management/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainDataManagementConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "main_data_management"
-    verbose_name = "Data Management"
